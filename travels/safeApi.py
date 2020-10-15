d177e306a7d5bae28de6ba376399ca4e3b26ae16
guptag911 <17ucc003@lnmiit.ac.in>
2020-10-15 19:24:17 +0530

    Date Added firebase

--- travels/safeApi.py
+++ travels/safeApi.py
@@ -9,24 +9,22 @@ BASE_DIR = os.getcwd()
 # Firebase Cursor connector
 # Place cred.json in static folder at the place where manage.py is.
 def my_cursor():
-	
 	try:
 		firebase_admin.get_app()
 	except ValueError as e:
-		cred = credentials.Certificate(os.path.join(BASE_DIR, "static", "travel", "cred.json"))
+		cred = credentials.Certificate(os.path.join(BASE_DIR, "cred.json"))
 		firebase_admin.initialize_app(cred)
 	
 	db = firestore.client()
 	return db
 
-
 # Return number of available seats between src and des for train no. "train"
-# There are 3 compartments in a train A1, A2 , A3 with 30 seats available for covid.
-# Actual Capactiy of each compartment os around 100
-def get_available_seats(src, des, train):
+# There are 3 compartments in a train AC1, AC2 , AC3 with 30 seats available for covid. i.e 33%
+# Actual Capactiy of each compartment os around 90
+def get_available_seats(src, des, train, date):
 	db = my_cursor()
-	doc_ref = db.collection("train").document(src).collection(train).document(src).collections()
-	
+	#doc_ref = db.collection("train").document(src).collection(train).document(src).collections()
+	doc_ref = db.collection("trains").document("users").collection(src).document(train).collection(des).document(str(date)).collections()
 	count = 0
 	for compartment in doc_ref:
 		for seats in compartment.stream():
@@ -34,34 +32,34 @@ def get_available_seats(src, des, train):
 	
 	return 90 - count
 
-
 # Handles Booking of tickets.
 # If that src and des are not ever travelled, it make a new entry
 # Return compartment number and seat number if booking is confirmed
 # Else it return None.
 def book_ticket(src, des, train, details):
 	db = my_cursor()
-	doc_ref = db.collection("train").document(src).collection(train).document(src)
+	#doc_ref = db.collection("train").document(src).collection(train).document(src)
+	doc_ref = db.collection("trains").document("users").collection(src).document(train).collection(des).document(str(date))
 	all_seats = doc_ref.get()
 	
 	if not all_seats.exists:		# Making default if not travelled.
 		doc_ref.set({
-			"A1": -2,
-			"A2": -2,
-			"A3": -2
+			"AC1": -1,
+			"AC2": -1,
+			"AC3": -1
 		})
 	all_seats = doc_ref.get()
 	
 	seats = all_seats.to_dict()
-	for comp in seats.keys():
-		fin_no = seats[comp]
-		if fin_no != 88:				# Final seat will be 88 as we start of with seat number 1  and we have 30 seats.
-			seat_no = fin_no + 3		# Vicinty of +-2 
-			doc_ref.collection(comp).document(str(seat_no)).set(details)
-			return comp, seat_no
-	
+	for pass_info in details:
+		for comp in seats.keys():
+			fin_no = seats[comp]
+			if fin_no != 89:				# Final seat will be 88 as we start of with seat number 1  and we have 30 seats.
+				seat_no = fin_no + 3		# Vicinty of +-2 
+				doc_ref.collection(comp).document(str(seat_no)).set(pass_info)
+				return comp, seat_no
+		
 	return None
 
-
-
-
+def get_history(userid):
+	
